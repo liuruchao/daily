496f87c71502e5080d23c40aa00f7e87dfff90a4
liuruchao <runlrc@163.com>
2019-05-14 20:12:18 +0800

    add 审核评语

--- daily_back/src/pages/HasAudit.vue
+++ daily_back/src/pages/HasAudit.vue
@@ -1,22 +1,30 @@
 <!--  -->
 <template>
   <div class='hasAudit'>
-      <h4 v-show="!datas.length">暂无已审核投稿</h4>
-      <div class="item" v-for="(item, index) in datas" :key="index">
-           <div>
-               <span>姓名：<span class="item--val">{{item.name}}</span></span>
-               <span>手机号：<span class="item--val">{{item.tel}}</span></span>
-           </div>
-           <div>
-               <span>邮箱：<span class="item--val">{{item.email}}</span></span>
-               <span>联系地址：<span class="item--val">{{item.adr}}</span></span>
-           </div>
-           <div>备注：<span style="width:80%" class="item--val">{{item.other}}</span></div>
-           <div>
-              <el-button type="primary" @click="check('http://localhost/daily_control/upload/' + item.filePath )">查看投稿</el-button>
-              <a><el-button type="primary">已审核</el-button></a>
-           </div>
+    <h4 v-show="!datas.length">暂无已审核投稿</h4>
+    <div class="item" v-for="(item, index) in datas" :key="index">
+      <div>
+        <span>姓名：<span class="item--val">{{item.name}}</span></span>
+        <span>手机号：<span class="item--val">{{item.tel}}</span></span>
       </div>
+      <div>
+        <span>邮箱：<span class="item--val">{{item.email}}</span></span>
+        <span>联系地址：<span class="item--val">{{item.adr}}</span></span>
+      </div>
+      <div>备注：<span style="width:80%" class="item--val">{{item.other}}</span></div>
+      <div>
+        <el-button type="primary" @click="check('http://localhost/daily_control/upload/' + item.filePath )">查看投稿
+        </el-button>
+        <el-button type="primary" @click='checkPy(index)'>查看评语</el-button>
+      </div>
+    </div>
+
+    <el-dialog title='评语' :visible.sync="dialogVisible" width="40%" :before-close="handleClose">  
+      <p style="padding:0">{{text}}</p>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">关闭</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -27,7 +35,8 @@
     components: {},
     data() {
       return {
-         datas:[]
+        datas: [],
+        dialogVisible:false
       };
     },
     //监听属性 类似于data概念
@@ -36,77 +45,87 @@
     watch: {},
     //方法集合
     methods: {
-        check(url){
-            window.open(url)
-            // window.location.href = 'www.baidu.com'
-        },
-        init(){
-            const that = this;
-            that.axios({
-            method: 'POST',
-            url: 'http://127.0.0.1:5000/hasaudit',
-            data: {
-            }
-          }).then(function (res) {
-              console.log(res.data)
-              that.datas = res.data;
-          })
-        }
+      check(url) {
+        window.open(url)
+        // window.location.href = 'www.baidu.com'
+      },
+      checkPy(index, id) {
+        this.text = this.datas[index]['evaluate'];
+        // alert(this.datas[index]['evaluate']);
+        this.dialogVisible = true;
+      },
+      init() {
+        const that = this;
+        that.axios({
+          method: 'POST',
+          url: 'http://127.0.0.1:5000/hasaudit',
+          data: {}
+        }).then(function (res) {
+          console.log(res.data)
+          that.datas = res.data;
+        })
+      }
     },
     //生命周期 - 创建完成（可以访问当前this实例）
     created() {
-       this.init();
+      this.init();
     },
   }
 
 </script>
-<style  scoped>
-   
-    .hasAudit{
-        background:#f7f7f7;
-        display:flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-        height:100vh;
-        overflow:auto;
-        padding:2em;
-        box-sizing: border-box;
-        align-items: flex-start;
-    }
-    h4{
-        text-align: center;
-    }
-    .item{
-        width:27%;
-        background:white;
-        min-height: 200px;
-        box-shadow: 2px 2px 5px #bfbaba;
-        border-radius: 5px;
-        /* margin: auto; */
-        padding:0.5em 1em;
-        color:#409EFF;
-        font-weight: bold;
-        margin-bottom:2em;
+<style scoped>
+  .hasAudit {
+    background: #f7f7f7;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    height: 100vh;
+    overflow: auto;
+    padding: 2em;
+    box-sizing: border-box;
+    align-items: stretch;
+    align-content: flex-start;
+  }
 
-    }
-    .item--val{
-         color:gray;
-         font-weight:400;
-    }
-    .item>div{
-        /* display: flex;
+  h4 {
+    text-align: center;
+  }
+
+  .item {
+    width: 27%;
+    background: white;
+    min-height: 200px;
+    box-shadow: 2px 2px 5px #bfbaba;
+    border-radius: 5px;
+    /* margin: auto; */
+    padding: 0.5em 1em;
+    color: #409EFF;
+    font-weight: bold;
+    margin-bottom: 2em;
+
+  }
+
+  .item--val {
+    color: gray;
+    font-weight: 400;
+  }
+
+  .item>div {
+    /* display: flex;
         justify-content: space-around; */
-        box-sizing:border-box;
-        margin:1em auto;
-    }
-    .item>div:last-of-type{
-        text-align: center;
-    }
-    .item>div>span{
-        width:47%;
-        display:inline-block;
-        box-sizing: border-box;
-        vertical-align: top;
-    }
-</style>
+    box-sizing: border-box;
+    margin: 1em auto;
+  }
 
+  .item>div:last-of-type {
+    text-align: center;
+  }
+
+  .item>div>span {
+    width: 47%;
+    display: inline-block;
+    box-sizing: border-box;
+    vertical-align: top;
+  }
+
+</style>
--- daily_back/src/pages/NoAudit.vue
+++ daily_back/src/pages/NoAudit.vue
@@ -1,22 +1,32 @@
 <!--  -->
 <template>
   <div class='noAudit'>
-      <h4 v-show="!datas.length">暂无未审核投稿</h4>
-      <div class="item" v-for="(item, index) in datas" :key="index">
-           <div>
-               <span>姓名：<span class="item--val">{{item.name}}</span></span>
-               <span>手机号：<span class="item--val">{{item.tel}}</span></span>
-           </div>
-           <div>
-               <span>邮箱：<span class="item--val">{{item.email}}</span></span>
-               <span>联系地址：<span class="item--val">{{item.adr}}</span></span>
-           </div>
-           <div>备注：<span style="width:80%" class="item--val">{{item.other}}</span></div>
-           <div>
-              <el-button type="primary" @click="check('http://localhost/daily_control/upload/' + item.filePath )">查看投稿</el-button>
-              <a><el-button type="primary" @click="audit(index,item.id)">确认审核</el-button></a>
-           </div>
+    <h4 v-show="!datas.length">暂无未审核投稿</h4>
+    <div class="item" v-for="(item, index) in datas" :key="index">
+      <div>
+        <span>姓名：<span class="item--val">{{item.name}}</span></span>
+        <span>手机号：<span class="item--val">{{item.tel}}</span></span>
       </div>
+      <div>
+        <span>邮箱：<span class="item--val">{{item.email}}</span></span>
+        <span>联系地址：<span class="item--val">{{item.adr}}</span></span>
+      </div>
+      <div>备注：<span style="width:80%" class="item--val">{{item.other}}</span></div>
+      <div>
+        <el-button type="primary" @click="check('http://localhost/daily_control/upload/' + item.filePath )">查看投稿
+        </el-button>
+        <el-button type="primary" @click="writePy(index,item.id)">审核评语</el-button>
+        <!-- <el-button type="primary" @click="audit(index,item.id)">确认审核</el-button> -->
+      </div>
+    </div>
+    <el-dialog :visible.sync="dialogVisible" width="40%" :before-close="handleClose">
+      <el-input type="textarea" :rows="3" placeholder="填写评语..." v-model="textarea">
+      </el-input>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="audit">提交</el-button>
+        <el-button type="primary" @click="dialogVisible = false">取消</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -27,7 +37,11 @@
     components: {},
     data() {
       return {
-         datas:[]
+        datas: [],
+        dialogVisible: false,
+        auditIndex:0,
+        auditId:0,
+        textarea:''
       };
     },
     //监听属性 类似于data概念
@@ -36,91 +50,105 @@
     watch: {},
     //方法集合
     methods: {
-        check(url){
-            window.open(url)
-            // window.location.href = 'www.baidu.com'
-        },
-        init(){
-            const that = this;
-            that.axios({
-            method: 'POST',
-            url: 'http://127.0.0.1:5000/noaudit',
-            data: {
-            }
-          }).then(function (res) {
-              console.log(res.data)
-              that.datas = res.data;
-          })
-        },
-        audit(index,id){
-            const that = this;
-            console.log('index',index);
-            console.log(id)
-            that.axios({
-            method: 'POST',
-            url: 'http://127.0.0.1:5000/audit',
-            data: {
-                id:id
-            }
-          }).then(function (res) {
-              if(res.data=='success'){
-                  that.datas.splice(index,1)
-              }
-          })
-        }
+      check(url) {
+        window.open(url)
+        // window.location.href = 'www.baidu.com'
+      },
+      init() {
+        const that = this;
+        that.axios({
+          method: 'POST',
+          url: 'http://127.0.0.1:5000/noaudit',
+          data: {}
+        }).then(function (res) {
+          console.log(res.data)
+          that.datas = res.data;
+        })
+      },
+      audit() {
+        let [index,id,textarea] = [this.auditIndex,this.auditId,this.textarea];
+        const that = this;
+        that.axios({
+          method: 'POST',
+          url: 'http://127.0.0.1:5000/audit',
+          data: {
+            id: id,
+            text: textarea
+          }
+        }).then(function (res) {
+          if (res.data == 'success') {
+            that.datas.splice(index, 1);
+            that.dialogVisible = false;
+            that.textarea = '';
+          }
+        })
+      },
+      writePy(index, id) {
+          [this.auditIndex,this.auditId] = [index,id];
+          this.dialogVisible = true;
+      }
     },
     //生命周期 - 创建完成（可以访问当前this实例）
     created() {
-       this.init();
+      this.init();
     },
   }
 
 </script>
-<style  scoped>
-   
-    .noAudit{
-        background:#f7f7f7;
-        display:flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-        height:100vh;
-        overflow:auto;
-        padding:2em;
-        box-sizing: border-box;
-        align-items: flex-start;
-    }
-    h4{
-        text-align: center;
-    }
-    .item{
-        width:27%;
-        background:white;
-        min-height: 200px;
-        box-shadow: 2px 2px 5px #bfbaba;
-        border-radius: 5px;
-        /* margin: auto; */
-        padding:0.5em 1em;
-        color:#409EFF;
-        font-weight: bold;
-        margin-bottom:2em;
-    }
-    .item--val{
-         color:gray;
-         font-weight:400;
-    }
-    .item>div{
-        /* display: flex;
+<style scoped>
+  .noAudit {
+    background: #f7f7f7;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    height: 100vh;
+    overflow: auto;
+    padding: 2em;
+    box-sizing: border-box;
+    align-items: stretch;
+    align-content: flex-start;
+  }
+
+  h4 {
+    text-align: center;
+  }
+
+  .item {
+    width: 27%;
+    background: white;
+    /* min-height: 200px; */
+    height:auto;
+    box-shadow: 2px 2px 5px #bfbaba;
+    border-radius: 5px;
+    /* margin: auto; */
+    padding: 0.5em 1em;
+    color: #409EFF;
+    font-weight: bold;
+    margin-bottom: 2em;
+    /* margin-right:1em; */
+  }
+
+  .item--val {
+    color: gray;
+    font-weight: 400;
+  }
+
+  .item>div {
+    /* display: flex;
         justify-content: space-around; */
-        box-sizing:border-box;
-        margin:1em auto;
-    }
-    .item>div:last-of-type{
-        text-align: center;
-    }
-    .item>div>span{
-        width:47%;
-        display:inline-block;
-        box-sizing: border-box;
-        vertical-align: top;
-    }
+    box-sizing: border-box;
+    margin: 1em auto;
+  }
+
+  .item>div:last-of-type {
+    text-align: center;
+  }
+
+  .item>div>span {
+    width: 47%;
+    display: inline-block;
+    box-sizing: border-box;
+    vertical-align: top;
+  }
+
 </style>
